2d05fce4aea32e0ff0e3a48362b0a4337f2ce2d9
Kulakov V.S <18kvs02@gmail.com>
2020-10-28 11:13:09 +0500

    ...

--- Hypothesis/Scripts/periodcheckertests.py
+++ Hypothesis/Scripts/periodcheckertests.py
@@ -2,20 +2,26 @@
 
 import os
 
-TEST = [
-    "x1 V -x2",
-    "-x1 V x2",
-    #"x1 V -x2-x3",
-    #"x2 V -x1-x3-x4",
-    #"x1 V -x2-x3-x4-x5",
-    "x1 V -x2-x3-x4-x5-x6",
-    "x2 V -x1-x3-x4-x5-x6-x7"
-    #"x1 V x2",
-    #"x1x2 V x3x4",
-    #"x1-x2 V -x3x4 V -x4-x5-x6 V x7-x1x2",
-    #"x1Vx2",
-    #"x1-x2V   -x3x4V-x4-x5-x6Vx7 -x1 x2"
-]
+TEST = []
+#TEST.append("x1 V -x2") #8
+#TEST.append("-x1 V x2") #8
+#TEST.append("x1 V -x2-x3") #16
+#TEST.append("x2 V -x1-x3-x4") #32
+#TEST.append("x1 V -x2-x3-x4-x5") #64
+#TEST.append("x1 V -x2-x3-x4-x5-x6") #128
+#TEST.append("x1 V x2x3 V -x4") #32
+#TEST.append("x1 V x2x3 V -x4-x5") #64
+#TEST.append("x1 V x2x4x6 V -x3-x5-x7") #231
+#TEST.append("x1x2 V -x3-x4") #32
+#TEST.append("x1x2 V x3x4 V x5x6 V -x7-x8") #502
+#TEST.append("x1x2 V x3x4 V x5x6 V -x7") #216
+#TEST.append("x1 V x2 V -x3") #16
+TEST.append("x1x3 V x2x4 V x7x5 V -x6-x8")
+TEST.append("-x1-x2-x3 V x4 V x5 V x6x7x8")
+
+
+def genTEST(size):
+    pass
 
 def main():
     for i in TEST:
