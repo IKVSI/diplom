1a0913781ff54507dff8065412b3f53e0d452a4d
Kulakov V.S <18kvs02@gmail.com>
2021-01-12 21:11:14 +0500

    ClearJPEG

---
+++ JPEGapp/jpegapp/JPEG/JPEG.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kvs on 10.01.2021.
+//
+
+#include "JPEG.h"
---
+++ JPEGapp/jpegapp/JPEG/JPEG.h
@@ -0,0 +1,14 @@
+//
+// Created by kvs on 10.01.2021.
+//
+
+#ifndef JPEGAPP_JPEG_H
+#define JPEGAPP_JPEG_H
+
+
+class JPEG {
+
+};
+
+
+#endif //JPEGAPP_JPEG_H
